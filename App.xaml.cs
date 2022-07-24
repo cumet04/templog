303a14174ea8b5005379dab285175bfe457533bb
cumet04 <medalhkr@gmail.com>
2022-07-24 12:55:58 +0900

    プロジェクト ファイルを追加します。

---
+++ App.xaml.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Threading;
+using System.Windows;
+
+namespace Tlog
+{
+    /// <summary>
+    /// Interaction logic for App.xaml.
+    /// </summary>
+    public partial class App : Application
+    {
+        private System.Windows.Forms.NotifyIcon? notifyIcon;
+        private Timer? timer;
+
+        /// <inheritdoc/>
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            base.OnStartup(e);
+
+            var icon = GetResourceStream(new Uri("icon.ico", UriKind.Relative)).Stream;
+            this.notifyIcon = new System.Windows.Forms.NotifyIcon
+            {
+                Visible = true,
+                Icon = new System.Drawing.Icon(icon),
+                ContextMenuStrip = this.Menu(),
+            };
+
+            this.timer = new Timer(
+                new TimerCallback(this.OnTimer),
+                new TemperatureCounter(),
+                0,
+                1000);
+        }
+
+        /// <inheritdoc/>
+        protected override void OnExit(ExitEventArgs e)
+        {
+            this.notifyIcon?.Dispose();
+            this.timer?.Dispose();
+            base.OnExit(e);
+        }
+
+        private void OnTimer(object? o)
+        {
+            var counter = o as TemperatureCounter;
+            if (counter == null) return;
+
+            var temperature = counter.GetValues()[0].value;
+
+            if (this.notifyIcon != null) this.notifyIcon.Text = temperature.ToString();
+        }
+
+        private System.Windows.Forms.ContextMenuStrip Menu()
+        {
+            var menu = new System.Windows.Forms.ContextMenuStrip();
+            menu.Items.Add("Exit", null, (object? sender, EventArgs e) => { this.Shutdown(); });
+            return menu;
+        }
+    }
+}
---
+++ TemperatureCounter.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections.Generic;
+using System.Diagnostics;
+
+namespace Tlog
+{
+    /// <summary>
+    /// 温度取得.
+    /// </summary>
+    [System.Runtime.Versioning.SupportedOSPlatform("Windows")]
+    internal class TemperatureCounter
+    {
+        private List<PerformanceCounter> counters = new List<PerformanceCounter>();
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="TemperatureCounter"/> class.
+        /// </summary>
+        public TemperatureCounter()
+        {
+            var categoryName = "Thermal Zone Information";
+            var counterName = "Temperature";
+
+            var cat = new PerformanceCounterCategory(categoryName);
+            var names = cat.GetInstanceNames();
+            foreach (var name in names)
+            {
+                this.counters.Add(new PerformanceCounter(categoryName, counterName, name));
+            }
+        }
+
+        /// <summary>
+        /// hoge.
+        /// </summary>
+        /// <returns>instanceごとのname/valueペア.</returns>
+        public (string name, float value)[] GetValues()
+        {
+            var results = new List<(string name, float value)>();
+            foreach (var counter in this.counters)
+            {
+                results.Add((name: counter.InstanceName, value: counter.NextValue()));
+            }
+
+            return results.ToArray();
+        }
+    }
+}
