172b96e8a7612d7f091ffa31b4a377709b588000
cumet04 <medalhkr@gmail.com>
2022-07-24 20:56:58 +0900

    現実的な時間にする

--- App.xaml.cs
+++ App.xaml.cs
@@ -29,7 +29,7 @@ namespace TempLog
                 new TimerCallback(this.OnTimer),
                 new TimerAction(),
                 0,
-                1000);
+                5 * 60 * 1000);
         }
 
         /// <inheritdoc/>
